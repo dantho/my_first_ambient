2686f03967d5dfca3770766002dc4bee04df39e2
Dan Thornton <dan.thornton+Git@thornton.me>
2023-03-04 16:08:48 -0500

    ignoring build, tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use ambient_api::{
     components::core::{
         game_objects::player_camera,
-        primitives::quad,
-        transform::{lookat_center, translation},
+        primitives::{quad,cube},
+        transform::{lookat_center, translation}, rendering::color,
+        player::player, physics::{box_collider, dynamic, physics_controlled}, prefab::prefab_from_url,
     },
     concepts::{make_perspective_infinite_reverse_camera, make_transformable},
-    prelude::*,
+    prelude::*, rand, entity::{AnimationController, AnimationAction},
 };
 
 #[main]
+/// My First Ambient Program
 pub async fn main() -> EventResult {
     Entity::new()
         .with_merge(make_perspective_infinite_reverse_camera())
@@ -17,12 +19,33 @@ pub async fn main() -> EventResult {
         .with(lookat_center(), vec3(0., 0., 0.))
         .spawn();
 
+    spawn_query(player()).bind(move |players| {
+        for _ in players {
+            Entity::new()
+                .with_merge(make_transformable())
+                .with_default(cube())
+                .with(translation(), rand::random())
+                .with(color(), rand::random())
+                .spawn();
+        }
+    });
+
     Entity::new()
         .with_merge(make_transformable())
-        .with_default(quad())
+        .with_default(cube())
+        .with(box_collider(), Vec3::ONE * 2.)
+        .with(dynamic(), true)
+        .with_default(physics_controlled())
         .spawn();
 
-    println!("Hello, Ambient!");
+    on(event::COLLISION, |c| {
+        println!("Collision");
+        EventOk
+    });
 
+    println!("Hello, Ambient!");
+    println!("Vec3::ZERO is '{:?}'", Vec3::ZERO);
+    println!("Vec3::ONE is '{:?}'", Vec3::ONE);
+    println!("Vec3::ONE * 5 is '{:?}'", Vec3::ONE * 5.);
     EventOk
 }
