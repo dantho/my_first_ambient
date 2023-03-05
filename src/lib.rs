d8b158268c92aeb0c106ec20238ee4772bbc41e8
Dan Thornton <dan.thornton+Git@thornton.me>
2023-03-04 21:47:23 -0500

    exploding blocks!

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 use ambient_api::{
     components::core::{
         game_objects::player_camera,
-        primitives::{quad,cube},
-        transform::{lookat_center, translation}, rendering::color,
-        player::player, physics::{box_collider, dynamic, physics_controlled}, prefab::prefab_from_url,
+        physics::{box_collider, dynamic, physics_controlled},
+        player::player,
+        prefab::prefab_from_url,
+        primitives::{cube, quad},
+        rendering::color,
+        transform::{lookat_center, translation},
     },
     concepts::{make_perspective_infinite_reverse_camera, make_transformable},
-    prelude::*, rand, entity::{AnimationController, AnimationAction},
+    entity::{AnimationAction, AnimationController},
+    prelude::*,
+    rand,
 };
 
 #[main]
 /// My First Ambient Program
 pub async fn main() -> EventResult {
-    let mut c = Vec4::new(0.0,0.0,0.0,0.0);
+    let mut c = Vec4::new(0.0, 0.0, 0.0, 0.0);
     let mut rng = rand::thread_rng();
 
     for x in c.as_mut() {
@@ -30,22 +35,22 @@ pub async fn main() -> EventResult {
     spawn_query(player()).bind(move |players| {
         for _ in players {
             Entity::new()
-                .with_merge(make_transformable())
                 .with_default(cube())
+                .with_merge(make_transformable())
                 .with(translation(), rand::random())
                 .with(color(), c)
                 .spawn();
         }
     });
-    
+
     for _ in 0..100 {
         for x in c.as_mut() {
             *x = rng.gen();
         }
         c.w = c.w.max(0.5);
         Entity::new()
-            .with_merge(make_transformable())
             .with_default(cube())
+            .with_merge(make_transformable())
             .with(box_collider(), Vec3::ONE * 2.)
             .with(dynamic(), true)
             .with_default(physics_controlled())
